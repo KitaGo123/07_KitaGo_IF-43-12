b64127236752bb44f3700dffc44441c639b9a1c3
ditanoviyanti <86896867+ditan91@users.noreply.github.com>
2022-06-20 19:46:39 +0700

    Update PenyediaJasa.php

--- app/Models/PenyediaJasa.php
+++ app/Models/PenyediaJasa.php
@@ -1,5 +1,9 @@
 <?php
+/*PENDAHULUAN
+Source code ini adalah melakukan pendefinisian model dari penyedia jasa dimana didalamnya
+id menjadi primary key dan id tersebut dilakukan auto increment*/
 
+/*Pemanggilan library*/
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,7 +11,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class PenyediaJasa extends Model
 {
+     /*memprotect variabel primary key sebagai id*/
     protected $primaryKey = 'id';
+    /*mendefinisikan auto increment */
     public $incrementing = true;
+    /*mendefinisikan factory untuk auto generate data peyedia jasa*/
     use HasFactory;
 }
