a54272ced4188c6a629d3bf08fad2bdc2bac17b0
ditanoviyanti <86896867+ditan91@users.noreply.github.com>
2022-06-20 19:44:46 +0700

    Update Customer.php

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -1,5 +1,9 @@
 <?php
+/*PENDAHULUAN
+Source code ini adalah melakukan pendefinisian model dari customer dimana didalamnya
+id menjadi primary key dan id tersebut dilakukan auto increment*/
 
+/*Pemanggilan library*/
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,9 +11,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    /*memprotect variabel usernameC*/
     protected $fillable = ['usernameC'];
-
+    /*memprotect variabel primary key sebagai id*/
     protected $primaryKey = 'id';
+    /*mendefinisikan auto increment */
     public $incrementing = true;
+    /*mendefinisikan factory untuk auto generate data Customer*/
     use HasFactory;
 }
